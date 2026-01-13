80b0a86d0f6eb369d2b47497b8b6035cf104c365
Ankitraj57 <ankitraj1320@gmail.com>
2026-01-13 22:44:45 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { expressMiddleware } from '@apollo/server/express4';
+import createApolloGraphqlServer  from './graphql'
+import UserService from './services/user';
+import { Token } from 'graphql';
+
+
+async function init () {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000;
+
+    app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.json({message: "Server is up and runing"});
+});
+
+app.use('/graphql',  
+    expressMiddleware(await createApolloGraphqlServer(), { context: async ({ req }) => {
+        // @ts-ignore
+        const token = req.headers["token"];
+        
+        try {
+            const user = UserService.decodeJWTToken(token as string);
+            return { user };
+        } catch (error) {
+            return {}
+        }
+      }
+    }
+  )
+);
+
+app.listen(PORT, () => console.log(`Server Started At PORT:${PORT}`));
+}
+
+init();
